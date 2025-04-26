f513068824d56919d65be7de8a30db87d5021df1
m0hamedKhalidMG <115906020+m0hamedKhalidMG@users.noreply.github.com>
2025-04-26 05:56:17 +0300

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -43,15 +43,15 @@ router.post("/delete/:train_no",requireRole('Admin'), async (req, res) => {
 router.post("/update",requireRole('Admin'), async (req, res) => {
     const { train_no, origin, destination, departure, arrival } = req.body;
   
-    try {
-      // await Train.updateOne(
-      //   { train_no },
-      //   { origin, destination, departure, arrival }
-      // );
-      res.redirect("/admin");
-    } catch (error) {
-      console.error("Error updating train:", error.message);
-      res.status(500).send("Failed to update train");
-    }
+    // try {
+    //   // await Train.updateOne(
+    //   //   { train_no },
+    //   //   { origin, destination, departure, arrival }
+    //   // );
+    //   res.redirect("/admin");
+    // } catch (error) {
+    //   console.error("Error updating train:", error.message);
+    //   res.status(500).send("Failed to update train");
+    // }
   });
 module.exports = router;
